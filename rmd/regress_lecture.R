c95820791cf72231edec9ffb62c5148a16e98daa
hubchev <DrStephanHuber@yahoo.com>
2022-04-12 12:10:36 +0200

    chg

---
+++ rmd/regress_lecture.R
@@ -0,0 +1,103 @@
+## ----setup, include=FALSE-----------------------------------------------------
+library("knitr")
+knit_hooks$set(purl = hook_purl)
+
+## ---- echo = TRUE-------------------------------------------------------------
+library("haven")
+classdata <- read.csv("https://raw.githubusercontent.com/hubchev/courses/main/dta/classdata.csv")
+
+head(classdata)
+
+## ---- echo = TRUE-------------------------------------------------------------
+
+summary(classdata)
+
+## ----pressure, echo=TRUE------------------------------------------------------
+library("ggplot2")
+ggplot(classdata, aes(x=height, y=weight)) + geom_point() 
+
+
+## ---- echo=TRUE---------------------------------------------------------------
+ggplot(classdata, aes(x=height, y=weight)) +
+  geom_point() +
+  stat_smooth(formula=y~x, method="lm", se=FALSE, colour="red", linetype=1)
+
+
+## ---- echo=TRUE---------------------------------------------------------------
+## baseline regression  model
+model  <- lm(weight ~ height + sex , data = classdata )
+show(model)
+interm <- model$coefficients[1] 
+slope  <- model$coefficients[2]
+interw <- model$coefficients[1]+model$coefficients[3] 
+
+## ---- echo=TRUE---------------------------------------------------------------
+ggplot(classdata, aes(x=height, y=weight, shape = sex)) +
+  geom_point() +
+  geom_abline(slope = slope, intercept = interw, linetype = 2, size=1.5)+
+  geom_abline(slope = slope, intercept = interm, linetype = 2, size=1.5) +
+  geom_abline(slope = coef(model)[[2]], intercept = coef(model)[[1]]) 
+
+
+## ---- echo=TRUE---------------------------------------------------------------
+
+ggplot(classdata, aes(x=height, y=weight, shape = sex)) +
+  geom_point( aes(size = 2)) +
+  stat_smooth(formula = y ~ x,  method = "lm", 
+              se = FALSE, colour = "red", linetype = 1)
+
+
+## ---- echo=TRUE---------------------------------------------------------------
+ggplot(classdata, aes(x=height, y=weight, shape = sex)) +
+  geom_point( aes(size = siblings)) 
+
+
+## ---- echo=FALSE--------------------------------------------------------------
+## baseline model
+model  <- lm(weight ~ height + sex , data = classdata )
+interw <- model$coefficients[1]+model$coefficients[3] 
+interm <- model$coefficients[1] 
+slope  <- model$coefficients[2]
+
+ggplot(classdata, aes(x=height, y=weight, shape = sex)) +
+  geom_point( aes(size = 2)) +
+  stat_smooth(formula = y ~ x,  
+              method = "lm", 
+              se = FALSE, 
+              colour = "red", 
+              linetype = 1)
+
+
+## ---- echo=TRUE, results='hide'-----------------------------------------------
+
+m1 <- lm(weight ~ height , data = classdata )
+m2 <- lm(weight ~ height + sex , data = classdata )
+m3 <- lm(weight ~ height + sex + height * sex , data = classdata )
+m4 <- lm(weight ~ height + sex + height * sex + siblings , data = classdata )
+m5 <- lm(weight ~ height + sex + height * sex , data = subset(classdata, siblings < 4 ))
+
+library(sjPlot)
+tab_model(m1, m2, m3, m4, m5,
+          p.style = "stars",
+          p.threshold = c(0.2, 0.1, 0.05),
+          show.ci = FALSE, 
+          show.se = FALSE) 
+
+## ---- echo=FALSE--------------------------------------------------------------
+tab_model(m1, m2, m3, m4, 
+          p.style = "stars",
+          p.threshold = c(0.2, 0.1, 0.05),
+          show.ci = FALSE, 
+          show.se = FALSE) 
+
+## ---- echo=FALSE--------------------------------------------------------------
+tab_model(m3, m5,
+          p.style = "stars",
+          p.threshold = c(0.2, 0.1, 0.05),
+          show.ci = FALSE, 
+          show.se = FALSE) 
+
+## ---- echo=T------------------------------------------------------------------
+plot(residuals(m3), fitted(m3))
+plot(residuals(m3), classdata$siblings)
+
