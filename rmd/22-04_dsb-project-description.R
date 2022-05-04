57484bef721b679f9d6974370fdbd124192edce5
hubchev <boitzen@yahoo.de>
2022-05-04 10:57:01 +0200

    minor changes

--- rmd/22-04_dsb-project-description.R
+++ rmd/22-04_dsb-project-description.R
@@ -1,4 +1,7 @@
-## ----setup, include=FALSE--------------------------------------------------------------------------------------------
+## ----setup, include=FALSE-----------------------------------------------------
 library("knitr")
 knit_hooks$set(purl = hook_purl)
+
+## ----eval=FALSE---------------------------------------------------------------
+#  rmarkdown::render("22-04_dsb-project-description.Rmd", "all")
 
